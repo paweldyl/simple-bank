3052eaa73620132658f792da6c5d752fc0e1838b
Paweł Dyl <hakiros54@gmail.com>
2025-03-15 19:37:32 +0100

    db queries and test initialized

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,77 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/techschool/simplebank/util"
+)
+
+func createRandomEntry(t *testing.T, account Account) Entry {
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, entry.AccountID, arg.AccountID)
+	require.Equal(t, entry.Amount, arg.Amount)
+
+	require.NotZero(t, entry.AccountID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	acc := createRandomAccount(t)
+	createRandomEntry(t, acc)
+}
+
+func TestGetEntry(t *testing.T) {
+	acc := createRandomAccount(t)
+	entry := createRandomEntry(t, acc)
+
+	fmt.Println(entry.AccountID)
+	fmt.Println(entry.Amount)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry.ID)
+	fmt.Println(entry.AccountID)
+	fmt.Println(entry.Amount)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry2.AccountID, entry.AccountID)
+	require.Equal(t, entry2.Amount, entry.Amount)
+	require.Equal(t, entry2.ID, entry.ID)
+
+	require.NotZero(t, entry2.AccountID)
+	require.NotZero(t, entry2.CreatedAt)
+	require.WithinDuration(t, entry.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestListEntries(t *testing.T) {
+	account := createRandomAccount(t)
+	for range 10 {
+		createRandomEntry(t, account)
+	}
+	entriesParams := ListEntriesParams{
+		AccountID: account.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+	entries, err := testQueries.ListEntries(context.Background(), entriesParams)
+	require.NoError(t, err)
+
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,79 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/techschool/simplebank/util"
+)
+
+func createRandomTransfer(t *testing.T, from Account, to Account) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: from.ID,
+		ToAccountID:   to.ID,
+		Amount:        util.RandomMoney(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, transfer.FromAccountID, arg.FromAccountID)
+	require.Equal(t, transfer.ToAccountID, arg.ToAccountID)
+	require.Equal(t, transfer.Amount, arg.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	from := createRandomAccount(t)
+	to := createRandomAccount(t)
+	createRandomTransfer(t, from, to)
+}
+
+func TestGetTransfer(t *testing.T) {
+	from := createRandomAccount(t)
+	to := createRandomAccount(t)
+	transfer := createRandomTransfer(t, from, to)
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer2.ID, transfer.ID)
+	require.Equal(t, transfer2.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, transfer2.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, transfer2.Amount, transfer.Amount)
+
+	require.NotZero(t, transfer2.CreatedAt)
+	require.WithinDuration(t, transfer.CreatedAt, transfer2.CreatedAt, time.Second)
+}
+
+func TestListTransfers(t *testing.T) {
+	from := createRandomAccount(t)
+	to := createRandomAccount(t)
+
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t, from, to)
+	}
+
+	arg := ListTransfersParams{
+		FromAccountID: from.ID,
+		ToAccountID:   to.ID,
+		Limit:         5,
+		Offset:        5,
+	}
+
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+	}
+}
